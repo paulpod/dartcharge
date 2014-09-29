cbd9ed2ae97597514b101f4c236f3f0c4b7de0e5
paulpod <paulpod@neuromantics.net>
2014-09-29 14:36:50 +0100

    mutli-type and links to existing site

--- app/routes.js
+++ app/routes.js
@@ -26,6 +26,31 @@ module.exports = {
   { author : 'Neale Donald Walsch', text : "You are afraid to die, and you're afraid to live. What a way to exist."}
 ];
 
+    app.get('/local-type', function (req, res) {
+      var o = req.query.Option;
+      
+      res.render('local-type-' + o, {'assetPath' : assetPath });
+
+      //res.send('Local Account Type: ' + o);  
+    });
+
+
+    app.get('/account-type', function (req, res) {
+      var o = req.query.Option;
+
+      if (o == 'personal') {
+        res.redirect('https://test.dart-charge.co.uk/Registration/Register');
+      } else if (o == 'local') {
+        res.render('dart-account-1-' + o, {'assetPath' : assetPath });
+      } else if (o == 'company') {
+        res.redirect('https://test.dart-charge.co.uk/Registration/RegisterBusiness');
+      }
+      
+      //res.render('dart-account-1-' + o, {'assetPath' : assetPath });
+
+      //res.send('Local Account Type: ' + o);  
+    });
+
 
 
   }
